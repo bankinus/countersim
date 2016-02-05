d07b16b97587949de40075614d2154da99a85a4b
Michael Banken <Michael.Banken@fau.de>
2016-02-05 15:14:30 +0100

    added code for lexer

---
+++ source/token.cpp
@@ -0,0 +1,22 @@
+#include "token.h"
+
+tokenType Token::get_type()
+{
+	return type;
+}
+void Token::set_type(tokenType t)
+{
+	type = t;
+}
+
+(const char *) Token::get_content();
+{
+	if (longcontent) return longcontent;
+	else return content;
+}
+
+void Token::set_content(const char *s)
+{
+	//TODO
+}
+
