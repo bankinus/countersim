eb0e79dfd9b2c6d3de972f13964935e09d255e8b
Michael Banken <Michael.Banken@fau.de>
2016-02-10 19:18:48 +0100

    changed interface of parser
    added some missing parsing functions
    parsing commands now requires a pointer pointer for the result

--- source/parser.h
+++ source/parser.h
@@ -1,14 +1,17 @@
 #pragma once
-#include "token.h"
 #include "simulator_command.h"
 
 bool parse_simulator_program(char *s);
 
+bool parse_config_command(char *s);
+
 bool parse_Minsky_program(char *s);
 
-bool parse_Minsky_routine(char *s);
+bool parse_URM_program(char *s);
 
-simulator_command parse_Minsky_command(char *s);
+bool parse_URM_routine(char *s);
 
-simulator_command parse_URM_command(char *s);
+bool parse_Minsky_command(char *s, simulator_command **res);
+
+bool parse_URM_command(char *s, simulator_command **res);
 
