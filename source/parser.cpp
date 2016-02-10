aa90d34e74051427a81535ff157e2f273c7bbd73
Michael Banken <Michael.Banken@fau.de>
2016-02-10 19:21:20 +0100

    added stubs for parser

---
+++ source/parser.cpp
@@ -0,0 +1,31 @@
+#include "parser.h"
+#include "token.h"
+
+
+bool parse_simulator_program(char *s) {
+	return false;
+}
+
+bool parse_config_command(char *s) {
+	return false;
+}
+bool parse_Minsky_program(char *s) {
+	return false;
+}
+
+bool parse_URM_program(char *s) {
+	return false;
+}
+
+bool parse_URM_routine(char *s) {
+	return false;
+}
+
+bool parse_Minsky_command(char *s, simulator_command **res) {
+	return false;
+}
+
+bool parse_URM_command(char *s, simulator_command** res) {
+	return false;
+}
+
