e1f2f3f5b48e685d727b9b465d4cdb88327ec187
Michael Banken <Michael.Banken@fau.de>
2016-02-10 15:44:03 +0100

    moved definition of debug functions to debug.cpp

--- source/debug.h
+++ source/debug.h
@@ -1,29 +1,4 @@
 #pragma once
-#include "token.h"
-#include "lexer.h"
-#include <stdio.h>
-#include <iostream>
 
-void lexertest() {
-	Token t;
-	char s[5000];
-	const char *pos;
-	while (1) {
-		if (fgets(s, 5000, stdin)==NULL) return;
-		pos = s;
-		while (1) {
-			Lexer::nextToken(pos, t, &pos);
-			std::cout << "<" << t.get_content() << ">";
-			std::cout.flush();
-			if (pos == NULL) {
-				std::cout << std::endl;
-				break;
-			}
-			if (t.get_type()==Newline) break;
-			if (t.get_type()==EOP) break;
-			if (t.get_type()==nil) break;
-		}
-	}
-	return;
-}
+void lexertest();
 
