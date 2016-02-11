0062ad82f9a72ff7fc9ec83b59fabaafa2469a3f
Michael Banken <Michael.Banken@fau.de>
2016-02-11 02:20:26 +0100

    added Subroutine context to command parsing

--- source/parser.cpp
+++ source/parser.cpp
@@ -1,31 +1,54 @@
 #include "parser.h"
 #include "token.h"
+#include "lexer.h"
+#include "simulator_command.h"
+#include <string>
 
+class Subroutine {
+	private:
+		Simulator_command *subprogram[];
+	public:
+		long long int first_line;
+		long long int last_line;
+		long long int current_line;
 
-bool parse_simulator_program(char *s) {
+		Subroutine () {}
+
+		long long int get_reg(std::string s) {
+			//TODO
+			return -1;
+		}
+
+		long long int get_command(std::string s) {
+			//TODO
+			return -1;
+		}
+};
+
+bool parse_simulator_program(const char *s) {
 	return false;
 }
 
-bool parse_config_command(char *s) {
+bool parse_config_command(const char *s) {
 	return false;
 }
-bool parse_Minsky_program(char *s) {
-	return false;
-}
-
-bool parse_URM_program(char *s) {
+bool parse_Minsky_program(const char *s) {
 	return false;
 }
 
-bool parse_URM_routine(char *s) {
+bool parse_URM_program(const char *s) {
 	return false;
 }
 
-bool parse_Minsky_command(char *s, Simulator_command **res) {
+bool parse_URM_routine(const char *s) {
 	return false;
 }
 
-bool parse_URM_command(char *s, Simulator_command** res) {
+bool parse_Minsky_command(const char *s, Simulator_command **res, Subroutine &sub) {
+	return false;
+}
+
+bool parse_URM_command(const char *s, Simulator_command** res, Subroutine &sub) {
 	return false;
 }
 
--- source/parser.h
+++ source/parser.h
@@ -1,17 +1,17 @@
 #pragma once
 #include "simulator_command.h"
 
-bool parse_simulator_program(char *s);
+bool parse_simulator_program(const char *s);
 
-bool parse_config_command(char *s);
+bool parse_config_command(const char *s);
 
-bool parse_Minsky_program(char *s);
+bool parse_Minsky_program(const char *s);
 
-bool parse_URM_program(char *s);
+bool parse_URM_program(const char *s);
 
-bool parse_URM_routine(char *s);
+bool parse_URM_routine(const char *s);
 
-bool parse_Minsky_command(char *s, Simulator_command **res);
+bool parse_Minsky_command(const char *s, Simulator_command **res);
 
-bool parse_URM_command(char *s, Simulator_command **res);
+bool parse_URM_command(const char *s, Simulator_command **res);
 
