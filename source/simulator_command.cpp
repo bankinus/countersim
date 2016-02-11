270c2fa2f8c4a87eb70763f81581abb9114934b2
Michael Banken <Michael.Banken@fau.de>
2016-02-11 02:12:00 +0100

    fixed inheritence issue and added some setters

--- source/simulator_command.cpp
+++ source/simulator_command.cpp
@@ -1,19 +1,39 @@
 #include "simulator_command.h"
 #include <sstream>
 
-void Madd::execute () {
+long long int Simulator_command::get_line () {
+	return line;
 }
 
-std::string  Madd::toString () {
+void Simulator_command::set_line (long long int i) {
+	line = i;
+}
+
+void Target_command::set_target(long long int number) {
+	target = number;
+}
+
+void Jump_command::set_jump(long long int number) {
+	next = number;
+}
+
+void Branch_command::set_branch(long long int number) {
+	branch = number;
+}
+
+void Madd_command::execute () {
+}
+
+std::string  Madd_command::toString () {
 	std::stringstream ss;
 	ss << "add " << "target" << " " << next;
 	return ss.str();
 }
 
-void Msub::execute () {
+void Msub_command::execute () {
 }
 
-std::string  Msub::toString () {
+std::string  Msub_command::toString () {
 	std::stringstream ss;
 	ss << "sub " << "target" << " " << next << " " << branch;
 	return ss.str();
--- source/simulator_command.h
+++ source/simulator_command.h
@@ -3,37 +3,52 @@
 
 class Simulator_command
 {
+	protected:
+		long long int line;
 	public:
 		virtual void execute () = 0;
 		virtual std::string toString() = 0;
+
+		long long int get_line();
+		void set_line(long long int i);
 };
 
-class Jump_command : public Simulator_command
+class Jump_command : virtual public Simulator_command
 {
 	protected:
-		Simulator_command *next;
+		long long int next;
+	public:
+		void set_jump(long long int branch);
 };
 
-class Target_command : public Simulator_command
+class Target_command : virtual public Simulator_command
 {
 	protected:
-		//TODO add target register
+		/* target is denoted by integer value
+		 * if target is positive register number equals target
+		 * if target number < 0 target is the negative number of the argument
+		 */
+		long long int target;
+	public:
+		void set_target(long long int number);
 };
 
-class Branch_command : public Simulator_command
+class Branch_command : virtual public Simulator_command
 {
 	protected:
-		Simulator_command *branch;
+		long long int branch;
+	public:
+		void set_branch(long long int branch);
 };
 
-class Madd : public Target_command, Jump_command
+class Madd_command : public Target_command, public Jump_command
 {
 	public:
 		virtual void execute ();
 		virtual std::string toString();
 };
 
-class Msub : public Target_command, Jump_command, Branch_command
+class Msub_command : public Target_command, public Jump_command, public Branch_command
 {
 	public:
 		virtual void execute ();
