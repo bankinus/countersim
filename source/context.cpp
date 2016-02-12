3c80480e646723dfc61f3e78532696b9fc06cb82
Michael Banken <Michael.Banken@fau.de>
2016-02-12 13:47:26 +0100

    added structure to hold identifier information in context
    also added the methods to access this data

--- source/context.cpp
+++ source/context.cpp
@@ -1,20 +1,18 @@
 #include "context.h"
 
 long long int Context::get_reg(std::string s) {
-	//TODO
-	return 0;
+	return reg_map[s];
 }
 
 void Context::set_reg(std::string s, long long int i) {
-
+	reg_map[s]=i;
 }
 
 long long int Context::get_line(std::string s) {
-	//TODO
-	return -1;
+	return reg_map[s];
 }
 
 void Context::set_line(std::string s, long long int i) {
-
+	line_map[s]=i;
 }
 
--- source/context.h
+++ source/context.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "simulator_command.h"
+#include <unordered_map>
 
 class Context {
 	private:
 		Simulator_command *subprogram[];
+		std::unordered_map<std::string, long long int> reg_map;
+		std::unordered_map<std::string, long long int> line_map;
 	public:
 		long long int first_line;
 		long long int last_line;
 		long long int current_line;
 
-		Context () {}
+		Context () : reg_map(), line_map(){}
 
 		long long int get_reg(std::string s);
 		void set_reg(std::string s, long long int i);
