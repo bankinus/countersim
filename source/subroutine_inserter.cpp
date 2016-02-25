4ecfb3a27e982db910925234d6e322f87e3d9bff
Michael Banken <Michael.Banken@fau.de>
2016-02-25 11:42:13 +0100

    added subroutine_inserter

---
+++ source/subroutine_inserter.cpp
@@ -0,0 +1,59 @@
+#include "subroutine_inserter.h"
+
+void Subroutine_inserter::visit(Madd_command *command) {
+	Madd_command *newcommand = new Madd_command();
+	if (command->get_target()>=0) {
+		newcommand->set_target(command->get_target());
+	}
+	else {
+		newcommand->set_target(registers[-(command->get_target())]);
+	}
+	if (command->get_jump()>=0) {
+		newcommand->set_jump(command->get_jump()+offset);
+	}
+	else {
+		if (exits[-(command->get_jump())]!=0) {
+			newcommand->set_jump(exits[-(command->get_jump())]);
+		}
+		else {
+			newcommand->set_jump_name(exit_names[-(command->get_jump())]);
+		}
+	}
+	parentcontext.add_command(newcommand);
+	parentcontext.current_line++;
+}
+
+void Subroutine_inserter::visit(Msub_command *command) {
+	Msub_command *newcommand = new Msub_command();
+	if (command->get_target()>=0) {
+		newcommand->set_target(command->get_target());
+	}
+	else {
+		newcommand->set_target(registers[-(command->get_target())]);
+	}
+	if (command->get_jump()>=0) {
+		newcommand->set_jump(command->get_jump()+offset);
+	}
+	else {
+		if (exits[-(command->get_jump())]!=0) {
+			newcommand->set_jump(exits[-(command->get_jump())]);
+		}
+		else {
+			newcommand->set_jump_name(exit_names[-(command->get_jump())]);
+		}
+	}
+	if (command->get_branch()>=0) {
+		newcommand->set_branch(command->get_jump()+offset);
+	}
+	else {
+		if (exits[-(command->get_branch())]!=0) {
+			newcommand->set_branch(exits[-(command->get_jump())]);
+		}
+		else {
+			newcommand->set_branch_name(exit_names[-(command->get_jump())]);
+		}
+	}
+	parentcontext.add_command(newcommand);
+	parentcontext.current_line++;
+}
+
---
+++ source/subroutine_inserter.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "routine_visitor.h"
+#include <vector>
+
+class Subroutine_inserter : public Routine_visitor {
+	protected:
+		long long int offset;
+		std::vector<int> registers;
+		std::vector<int> exits;
+		std::vector<std::string> exit_names;
+		Context &parentcontext;
+	public:
+		Subroutine_inserter(Context &c, std::vector<int> regs, std::vector<int> ex, std::vector<std::string> en) : parentcontext(c), registers(regs), exits(ex), exit_names(en){
+			offset = parentcontext.current_line;
+		}
+		virtual void visit(Madd_command *command) override;
+		virtual void visit(Msub_command *command) override;
+};
+
