9bd90c2a285a46f1c6ae52c54ad7e5171f53a46e
Michael Banken <Michael.Banken@fau.de>
2016-02-25 11:46:06 +0100

    fixed mistakes in name resolution

--- source/routine_name_resolver.cpp
+++ source/routine_name_resolver.cpp
@@ -5,7 +5,7 @@ void Routine_name_resolver::visit(Madd_command *command) {
 		command->set_target(context.get_reg(command->get_target_name()));
 	}
 	if (command->get_jump_name()!="") {
-		command->set_jump(context.get_reg(command->get_jump_name()));
+		command->set_jump(context.get_line(command->get_jump_name()));
 	}
 }
 
@@ -14,10 +14,10 @@ void Routine_name_resolver::visit(Msub_command *command) {
 		command->set_target(context.get_reg(command->get_target_name()));
 	}
 	if (command->get_jump_name()!="") {
-		command->set_jump(context.get_reg(command->get_jump_name()));
+		command->set_jump(context.get_line(command->get_jump_name()));
 	}
 	if (command->get_branch_name()!="") {
-		command->set_branch(context.get_reg(command->get_branch_name()));
+		command->set_branch(context.get_line(command->get_branch_name()));
 	}
 }
 
