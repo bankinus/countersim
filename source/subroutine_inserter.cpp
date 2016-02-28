d2f1282f768ff549515ec9e23c8532d4a4aef7d6
Michael Banken <Michael.Banken@fau.de>
2016-02-28 08:57:09 +0100

    fixed branch and line setting in Subroutine_inserter

--- source/subroutine_inserter.cpp
+++ source/subroutine_inserter.cpp
@@ -1,7 +1,8 @@
 #include "subroutine_inserter.h"
-
+#include "debug.h"
 void Subroutine_inserter::visit(Madd_command *command) {
 	Madd_command *newcommand = new Madd_command();
+	newcommand->set_line(command->get_line());
 	if (command->get_target()>=0) {
 		newcommand->set_target(command->get_target());
 	}
@@ -25,6 +26,7 @@ void Subroutine_inserter::visit(Madd_command *command) {
 
 void Subroutine_inserter::visit(Msub_command *command) {
 	Msub_command *newcommand = new Msub_command();
+	newcommand->set_line(command->get_line());
 	if (command->get_target()>=0) {
 		newcommand->set_target(command->get_target());
 	}
@@ -47,10 +49,10 @@ void Subroutine_inserter::visit(Msub_command *command) {
 	}
 	else {
 		if (exits[-(command->get_branch())]!=0) {
-			newcommand->set_branch(exits[-(command->get_jump())]);
+			newcommand->set_branch(exits[-(command->get_branch())]);
 		}
 		else {
-			newcommand->set_branch_name(exit_names[-(command->get_jump())]);
+			newcommand->set_branch_name(exit_names[-(command->get_branch())]);
 		}
 	}
 	parentcontext.add_command(newcommand);
