14346a12e5986a2e02093ed5cacc16e4f55916a7
Michael Banken <Michael.Banken@fau.de>
2016-02-28 08:56:14 +0100

    added handling of _next to Routine_name_resolver

--- source/routine_name_resolver.cpp
+++ source/routine_name_resolver.cpp
@@ -1,11 +1,17 @@
 #include "routine_name_resolver.h"
+#include "debug.h"
 
 void Routine_name_resolver::visit(Madd_command *command) {
 	if (command->get_target_name()!="") {
 		command->set_target(context.get_reg(command->get_target_name()));
 	}
 	if (command->get_jump_name()!="") {
-		command->set_jump(context.get_line(command->get_jump_name()));
+		if (command->get_jump_name()=="_next") {
+			command->set_jump(command->get_line()+1);
+		}
+		else {
+			command->set_jump(context.get_line(command->get_jump_name()));
+		}
 	}
 }
 
@@ -14,7 +20,12 @@ void Routine_name_resolver::visit(Msub_command *command) {
 		command->set_target(context.get_reg(command->get_target_name()));
 	}
 	if (command->get_jump_name()!="") {
-		command->set_jump(context.get_line(command->get_jump_name()));
+		if (command->get_jump_name()=="_next") {
+			command->set_jump(command->get_line()+1);
+		}
+		else {
+			command->set_jump(context.get_line(command->get_jump_name()));
+		}
 	}
 	if (command->get_branch_name()!="") {
 		command->set_branch(context.get_line(command->get_branch_name()));
