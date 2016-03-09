77bf447672cd8bacd3341873ebf438f1df530d51
Michael Banken <Michael.Banken@fau.de>
2016-03-09 11:47:29 +0100

    implemented the console interface

--- source/countermachine.cpp
+++ source/countermachine.cpp
@@ -1,7 +1,12 @@
 #include "lexer.h"
 #include "debug.h"
+#include "simulation.h"
+#include "context.h"
+#include "parser.h"
+#include "execution_visitor.h"
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
@@ -11,11 +16,12 @@ namespace po = boost::program_options;
 
 int main (int argc, char ** argv) {
 	//Declare the supported options.
-	po::options_description description("Allowed options");
+	po::options_description description("useage: countermachine [program] [register-presets] [options]\nAllowed options");
 	description.add_options()
 		("input-file", po::value<std::string>(), "path to file containing the program")
 		("register-file", po::value<std::string>(), "path to file containing register inputs")
 		("gui,g", "if this flag is set program simulator runs in graphical mode")
+		("run,r", "if this flag is set program simulator runs program to completion upon start")
 		("help,h", "produce help message")
 		;
 	
@@ -44,13 +50,95 @@ int main (int argc, char ** argv) {
 	} else {
 		if (!v_map.count("gui")) {
 			std::cerr << "error: no inputfile specified in non graphical mode\n";
+			return 0;
 		}
 	}
 
 	if (v_map.count("gui")) {
+		//run in console mode
+		//TODO
 		std::cerr << "error: gui not yet implemented\n";
 		return 0;
 	}
+	else {
+		//run in console mode
+		Simulation simulation;
+		std::string program;
+		//load program
+		std::ifstream filestream(v_map["input-file"].as<std::string>());
+		if (!filestream.is_open()) {
+			std::cerr << "error: could not open file:" << v_map["input-file"].as<std::string>() << "\n";
+			return 0;
+		}
+		std::stringstream bufstream;
+		bufstream << filestream.rdbuf();
+		program = bufstream.str();
+		//parse program
+		Context *context = Parser().parse_simulator_program(program.c_str());
+		if (context==NULL) return 0;
+		//enter registers
+		if (v_map.count("register-file")) {
+			std::ifstream reg_in_stream(v_map["input-file"].as<std::string>());
+			if (!reg_in_stream.is_open()) {
+				std::cerr << "error: could not open file:" << v_map["input-file"].as<std::string>() << "\n";
+				return 0;
+			}
+			for (size_t i=0; !reg_in_stream.eof(); i++){
+				unsigned long long int val;
+				reg_in_stream >> val;
+				if (reg_in_stream.eof()) break;
+				simulation.get_register(i)->set_value(val);
+			}
+		}
+		else {
+			for (size_t i=0; !std::cin.eof() && (i<simulation.get_max_reg() || simulation.get_max_reg()==0); i++){
+				unsigned long long int val;
+				std::cin >> val;
+				if (std::cin.eof()) break;
+				simulation.get_register(i)->set_value(val);
+				std::cin.clear();
+			}
+		}
+		//run program
+		Execution_visitor exe(simulation);
+		if (v_map.count("run")) {
+			exe.visitc(*context);
+		}
+		else {
+			bool done;
+			while (!done && !std::cin.eof()) {
+				char cmd;
+				std::cin >> cmd;
+				switch (cmd) {
+					case 's':
+						done = exe.step_visitc(*context);
+						//print registers at end of execution
+						std::cout << "register content after step:" << std::endl;
+						for (const Register &reg: simulation.get_registers()) {
+							std::cout << reg.get_value() << std::endl;
+						}
+						break;
+					case 'r': 
+						exe.visitc(*context);
+						done = true;
+						break;
+					case 'q':
+						return 0;
+						break;
+					default:
+						std::cerr << "no valid command:" << cmd << std::endl;
+						break;
+				}
+			}
+		}
+		//print registers at end of execution
+		std::cout << "execution terminated with the following register contents:" << std::endl;
+		for (const Register &reg: simulation.get_registers()) {
+			std::cout << reg.get_value() << std::endl;
+		}
+		
+		return 1;
+	}
 
-	return 1;
+	return 0;
 }
